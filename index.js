8247e12366743985668b6494bae962b9bceaf82d
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-11-01 20:23:33 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cookieParser = require("cookie-parser");
 
 const port = process.env.PORT || 5000;
 
 require("dotenv").config();
 
 //middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 //mondb
 
@@ -24,6 +32,9 @@ const client = new MongoClient(uri, {
   },
 });
 
+//our middlewares
+// const logger 
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -32,6 +43,21 @@ async function run() {
     const serviceCollection = client.db("carDoctor").collection("services");
     const BookingCollection = client.db("carDoctor").collection("bookings");
 
+    //auth related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          // sameSite: "none",
+        })
+        .send({ success: true });
+    });
+
+    //services
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
@@ -52,6 +78,7 @@ async function run() {
 
     //bookings
     app.get("/bookings", async (req, res) => {
+      console.log("dsfd token", req.cookies.token);
       let query = {};
       if (req.query.email) {
         query = { email: req.query.email };
@@ -76,7 +103,7 @@ async function run() {
           status: updateBooking.status,
         },
       };
-      const result = await BookingCollection.updateOne(filter,updateDoc);
+      const result = await BookingCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
 
